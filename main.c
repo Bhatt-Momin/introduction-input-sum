d2134c7b2ebc66778482e21ed916658aab7519cb
Momin <mominnazir191201@gmail.com>
2025-04-22 22:56:23 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+   printf("this is my first program \n And i want all of you to run me \t on your respective pcs");
+   int a = 3, b = 3, c=5;
+  int sum = a+b+c;
+  printf("\n enter the numbers: %d, %d, %d"  ,a,b,c);
+  printf("\n sum of numbers is %d" , sum);
+  int x,y,z, multiply =0;
+  printf("\n enter the three numbers:");
+  scanf("%d,%d,%d", &x,&y,&z);
+  multiply = x+y+z;
+  printf("thw sum of the no is %d",multiply);
+return 0;
+}
